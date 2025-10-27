ea8e10df7fd942db082a0b3184e177c77ad8769b
marthinisundar <115288319+marthinisundar@users.noreply.github.com>
2025-10-27 19:23:09 +0530

    Update dataframe display to use container width

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-st_df=st.dataframe(data=smoothiefroot_response.json())
-# ,use_width_container=true)
+st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
